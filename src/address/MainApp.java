d4107ea8320d53260b71709fc69b732e9a23800d
Ольга Гузева <guzeva@bipartner.ru>
2017-04-12 01:05:04 +0300

    darkTheme

--- src/address/MainApp.java
+++ src/address/MainApp.java
@@ -1,8 +1,16 @@
 package address;
 
+import java.io.File;
 import java.io.IOException;
+import java.security.SecurityPermission;
+import java.util.prefs.Preferences;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 
 import address.model.Person;
+import address.model.PersonListWrapper;
 import address.view.PersonEditDialogController;
 import address.view.PersonOverviewController;
 import javafx.application.Application;
@@ -10,6 +18,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -54,9 +65,12 @@ public class MainApp extends Application {
 		this.primaryStage = primaryStage;
 		this.primaryStage.setTitle("AddressApp");
 		
+		//устанавливаем иконку для приложения
+		this.primaryStage.getIcons().add(new Image("file:resources/images/Death_Note.png"));
+		
 		initRootLayout();
 		
-		showPersonOverview();
+		showPersonOverview();		
 	}
 	
 	/*������������� ��������� ������*/
@@ -143,4 +157,103 @@ public class MainApp extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
+	/**
+	 * Возвращает preference файла адресатов, то есть, последний открытый файл.
+	 * Этот preference считывается из реестра, специфичного для конкретной
+	 * операционной системы. Если preference не был найден, то возвращается null.
+	 * 
+	 * @return
+	 */
+	public File getPersonFilePath(){
+		Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+		String filePath = prefs.get("filePath", null);
+		if(filePath != null){
+			return new File(filePath);
+		}else{
+			return null;
+		}
+	}
+	
+	/**
+	 * Задаёт путь текущему загруженному файлу. Этот путь сохраняется
+	 * в реестре, специфичном для конкретной операционной системы.
+	 * 
+	 * @param file - файл или null, чтобы удалить путь
+	 */
+	public void setPersonFilePath(File file){
+		Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+		if(file != null){
+			prefs.put("filePath", file.getPath());
+			
+			//Обновление заглавия сцены
+			primaryStage.setTitle("AddressApp - " + file.getName());
+		}else{
+			prefs.remove("filePath");
+			
+			//Обновление заглавия сцены
+			primaryStage.setTitle("AddressApp");
+		}
+	}
+	
+	/**
+	 * Загружает информацию об адресатах из указанного файла.
+	 * Текущая информация об адресатах будет заменена.
+	 * 
+	 * @param file
+	 */
+	public void loadPersonDataFromFile(File file){
+		try{
+			JAXBContext context = JAXBContext.newInstance(PersonListWrapper.class);
+			Unmarshaller um = context.createUnmarshaller();
+			
+			//Чтение XML из файла и демаршализация
+			PersonListWrapper wrapper = (PersonListWrapper) um.unmarshal(file);
+			
+			personData.clear();
+			personData.addAll(wrapper.getPersons());
+			
+			//Сохраняем путь к файлу в реестре
+			setPersonFilePath(file);
+		}catch(Exception e){
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Error");
+			alert.setHeaderText("Could not load data");
+			alert.setContentText("Could not load data from file:\n" + file.getPath());
+			
+			alert.showAndWait();
+		}
+	}
+	
+	/**
+	 * Сохраняет текущую информацию об адресатах в указанном файле.
+	 * 
+	 * @param file
+	 */
+	public void saxePersonDataToFile(File file){
+		try{
+			JAXBContext context = JAXBContext.newInstance(PersonListWrapper.class);
+			Marshaller m = context.createMarshaller();
+			m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			
+			//Обертываем наши данные об адресатах
+			PersonListWrapper wrapper = new PersonListWrapper();
+			wrapper.setPersons(personData);
+			
+			//Маршаллируем и сохраняем XML файл
+			m.marshal(wrapper, file);
+			
+			//Сохраняем путь к файлу
+			setPersonFilePath(file);
+		}catch(Exception e){
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Error");
+			alert.setHeaderText("Could not save data");
+			alert.setContentText("Could not save data to file:\n" + file.getPath());
+			
+			alert.showAndWait();
+		}
+	}
+	
+	
 }
